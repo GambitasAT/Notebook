b3280a677d6d96f59c3ff01220e471f7a1420c5d
Augustas Tupciauskas <augustast@icloud.com>
2024-08-16 15:17:39 +0300

    Add project files.

---
+++ Notebook/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notebook.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public byte[] PasswordHash { get; set; }
+        [Required]
+        public byte[] PasswordSalt { get; set; }
+    }
+}
